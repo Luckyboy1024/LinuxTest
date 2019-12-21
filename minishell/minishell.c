b2039a27a2b368977877d8eb6ae6ec4f88021188
keybug <2819549538@qq.com>
2019-12-21 23:54:15 +0800

    2019-12-21:迷你shell程序

---
+++ minishell/minishell.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdint.h>
+
+int main()
+{
+    while(1){
+        printf("[user@localhost] $ ");
+        fflush(stdout);
+        char cmd_buf[1024] = { 0 };
+        
+    }
+    return 0;
+}
