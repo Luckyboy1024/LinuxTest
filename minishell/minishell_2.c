2ea43fee451b36cdf625e2a491d0271c5e10d725
keybug <git@gitee.com:keybug/Linux_test.git>
2020-02-11 21:27:13 +0800

    2020-02-25:代码修改:minishell版本2.2020.02.25更新

--- minishell/minishell_2.c
+++ minishell/minishell_2.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdint.h>
 #include <ctype.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 
 char g_Command[1024];
 
@@ -11,8 +13,7 @@ int GetCommand()
     memset(g_Command, '\0', sizeof(g_Command));
     printf("[username@localhost] $ ");
     fflush(stdout);
-
-    if(!fgets(g_Command, sizeof(g_Command) - 1), stdin)
+    if(!fgets(g_Command, sizeof(g_Command) - 1, stdin))
     {
         printf("fgets error\n");
         return -1;
@@ -24,27 +25,49 @@ int GetCommand()
     return 0;
 }
 
-int ExecCommand()
+int ExecCommand(char* argv[])
 {
-
+    if (NULL == argv[0])
+    {
+        printf("ExecCommand error\n");
+        return -1;                        
+    }
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        perror("fork");
+        return -1;                                
+    }
+    else if (0 == pid)
+    {
+        // child
+        execvp(argv[0], argv);
+        exit(0);
+    }
+    else
+    {
+        // father
+        waitpid(pid, NULL, 0);
+    }
+    return 0;
 }
 
 int DealCommand(char* command)
 {
-    if(!command || '\0' == *command)
+    if(!command || *command == '\0')
     {
         printf("command error\n");
         return -1;
     }
     int argc = 0;
-    char argv[32] = { 0 };
+    char* argv[32] = { 0 };
     while(*command)
     {
         if(!isspace(*command))
         {
             argv[argc] = command;
             argc++;
-            while(!isspace(*command) && '\0' != *command)
+            while((!isspace(*command)) && ('\0' != *command))
             {
                 command++;
             }
@@ -57,6 +80,7 @@ int DealCommand(char* command)
     {
         printf("argv[%d] [%s]\n", i, argv[i]);
     }
+    ExecCommand(argv);
     return 0;
 }
 
