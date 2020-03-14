3952394c98d865c665abb5652116d41c3972c7b1
keybug <2819549538@qq.com>
2020-03-14 09:51:29 +0800

    2020-03-13:代码提交:makefile-project

---
+++ makefile-project/main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void test()
+{
+    printf("hehe\n");
+}
+
+int main()
+{
+    printf("hehe\n");
+    test();
+    return f();
+}
+
---
+++ makefile-project/test.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdlib.h>
+
+int f()
+{
+    test();
+    return 0;
+}
