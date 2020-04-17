e3252e8ed215a2f9a29197945a60e2494590f0a6
keybug <2819549538@qq.com>
2020-04-17 11:20:12 +0800

    2020-04-17:代码提交: test-20-04-17

--- test-20-04/test-20-04-17/srcs/9.c
+++ test-20-04/test-20-04-17/srcs/9.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
-#include <ctype.h>	// isprint()
+
 int main()
 {
-	char str[] = "a5 @#;";
-	int i = 0;
-	for (i = 0; str[i] != 0; i++)
-	{
-		if (isprint(str[i]))
-			printf("str[%d] is printable character : %d\n", i, str[i]);
-	}
 	return 0; 
 }
 
