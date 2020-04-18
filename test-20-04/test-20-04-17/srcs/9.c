93ed1c6a92e7656b37b744fd92f4011370cb78de
keybug <2819549538@qq.com>
2020-04-18 21:56:18 +0800

    2020-04-18:提交代码: test-20-04-17

--- test-20-04/test-20-04-17/srcs/9.c
+++ test-20-04/test-20-04-17/srcs/9.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-
+#include <ctype.h>
 int main()
 {
+	char str[] = "123c@# FDs[e?]";
+	int i = 0;
+	for (i = 0; str[i] != 0; i++)
+	{
+		if (isxdigit(str[i]))
+			printf("%c is a hexadecimal digits\n", str[i]);
+	}
 	return 0; 
 }
 
