3195fdefa0fc3d678cffb2df0d832a97b69af07b
keybug <2819549538@qq.com>
2020-04-19 21:40:25 +0800

    2020-04-19:提交代码: test-20-04-19

--- test-20-04/test-20-04-19/srcs/8.c
+++ test-20-04/test-20-04-19/srcs/8.c
@@ -4,8 +4,8 @@ int main()
 {
 	double a = 123.45;
 	double b = -1234.56;
-	char *ptr = NULL;
-	ptr = gcvt(a, 5, ptr);
+	char *ptr = (char *)malloc(sizeof(char*));
+	gcvt(a, 5, ptr);
 	printf("a_value = %s\n", ptr);
 	ptr = gcvt(b, 6, ptr);
 	printf("b_value = %s\n", ptr);
