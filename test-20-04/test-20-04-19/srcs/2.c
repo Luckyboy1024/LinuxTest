bc5e9fbe0675594d0df663e58d5c4c76148a186a
keybug <2819549538@qq.com>
2020-04-19 11:31:23 +0800

    2020-04-19:提交代码: test-20-04-19

--- test-20-04/test-20-04-19/srcs/2.c
+++ test-20-04/test-20-04-19/srcs/2.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main()
 {
+	double a = 123.45;
+	double b = -1234.567;
+	char *ptr = NULL;
+	int decpt, sign = 0;
+	ptr = fcvt(a, 2, &decpt, &sign);	// 小数点后显示 2 位数
+	printf("decept = %d, sign = %d, a_value = %s\n", decpt, sign, ptr);
+	ptr = fcvt(b, 3, &decpt, &sign);	// 小数点后显示 3 位数
+	printf("decept = %d, sign = %d, b_value = %s\n", decpt, sign, ptr);
 	return 0; 
 }
 
