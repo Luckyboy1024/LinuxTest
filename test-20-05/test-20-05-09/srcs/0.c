53c31a7256b1157d4c58293a574d80784276765f
keybug <2819549538@qq.com>
2020-05-09 15:45:25 +0800

    2020-05-09 : 提交代码 : test-20-05-09

---
+++ test-20-05/test-20-05-09/srcs/0.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int answer = 0;
+	answer = abs(-12);
+	printf(" | -12 | = %d\n", answer);
+	return 0; 
+}
+
+	
---
+++ test-20-05/test-20-05-09/srcs/5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	double angle = 0.0;
+	angle = asin(0.5);
+	printf("angle = %f\n", angle);
+	return 0; 
+}
+
+	
