97946d4dffeb8c585f777b9ad79b58629678dce8
keybug <2819549538@qq.com>
2021-02-22 23:49:46 +0800

    2021-02-22 更新

--- project/project-ChatSystem/ProtocolUtil.hpp
+++ project/project-ChatSystem/ProtocolUtil.hpp
@@ -1,5 +1,7 @@
 #pragma once 
+
 #include <iostream>
+#include "Log.hpp"
 
 class SocketApi{
 	public:
@@ -8,7 +10,26 @@ class SocketApi{
 			int sock = socket(AF_INET, type, 0);
 			if(sock < 0)
 			{
-
+				// 打印日志
+				LOG("socket error!", ERROR);
+				exit(1);
 			}
+		}
+		static void Bind(int sock, int port)	// 绑定端口号和套接字
+		{
+			struct Sockaddr_in local;
+			local.sin_family = AF_INET;
+			local.sin_addr.s_addr = htonl(INADDR_ANY);  
+			local.sin_port = htons(port);
+
+			if(bind(sock, (struct sockaddr*)&local, sizeof(local)) < 0)	// 绑定失败
+			{
+				LOG("socket error", ERROR);
+				exit(2);
+			}
+		}
+		static void Listen(int sock)
+		{
+
 		}
 }
