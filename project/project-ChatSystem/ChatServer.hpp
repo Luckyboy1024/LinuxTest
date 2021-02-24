d410e711c7a283db4f9ca3c66ae91e507e03a1e7
keybug <2819549538@qq.com>
2021-02-24 21:30:14 +0800

    2021-02-24 补充

--- project/project-ChatSystem/ChatServer.hpp
+++ project/project-ChatSystem/ChatServer.hpp
@@ -1,12 +1,46 @@
 #pragma once 
 
 #include <iostream>
-
-
+#include <pthread.h>
+#include "ProtocolUtil.hpp"
 
 class ChatServer{
 	private:
-		int listen_sock;
-		int port;
+		int tcp_listen_sock;
+		int tcp_port;
+		int udp_work_sock; 
+		int udp_port;
+	public:
+		ChatServer(int tcp_port_ = 8080, int udp_port_ = 8088):
+			tcp_port(tcp_port_),
+			tcp_listen_sock(-1),
+			udp_port(udp_port_),
+			udp_work_sock(-1)
+		{}
+		void InitServer()
+		{
+			tcp_listen_sock = SocketApi::Socket(SOCK_STREAM);
+			SocketApi::Bind(tcp_listen_sock, tcp_port);
+			SocketApi::Bind(udp_work_sock, udp_port);
 
+			SocketApi::Listen(tcp_listen_sock);
+
+			// 创建三个线程
+			pthread_t tid;
+			pthread_create(&tid, UserManager, );
+			pthread_create(&tid, RecvMessage, );
+			pthread_create(&tid, SendMessage, );
+			for(;;){
+				int sock = SocketApi::Accept(tcp_listen_sock, ip, port);
+				if(sock > 0)
+				{
+					std::cout << "get a new client " << ip << " : " << port << std::endl;
+
+				}
+			}
+		}
+		~ChatServer()
+		{
+
+		}
 };
--- project/project-ChatSystem/ProtocolUtil.hpp
+++ project/project-ChatSystem/ProtocolUtil.hpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 #include <string>
+#include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
